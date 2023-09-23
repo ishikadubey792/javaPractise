96ff11a1b1998454c10f71e8d09e99c28980aa21
Ishika <128132667+ishikadubey792@users.noreply.github.com>
2023-09-23 17:44:36 +0000

    postorderSuccessor

---
+++ postorderSuccessor.java
@@ -0,0 +1,57 @@
+import java.util.* ;
+import java.io.*; 
+
+/*******************************************************
+Following is the BinaryTreeNode class structure
+
+class BinaryTreeNode<T> {
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+    
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+}
+*******************************************************/
+
+public class Solution
+{    
+	public static int postOrderSuccessor(BinaryTreeNode<Integer> root, int M)
+	{
+        /* 
+		  Your class should be named Solution.
+	 	  Read input as specified in the question.
+	 	  Print output as specified in the question.
+		*/
+         if(root==null){
+             return -1;
+         }
+         
+         ArrayList<Integer> arr = new ArrayList<>();
+         postOrder(root , arr);
+          
+          for (int i = 0; i < arr.size(); i++) {
+            int n = arr.get(i);
+            if (n == M && i < arr.size() - 1) {
+                int ans = arr.get(i + 1);
+                return ans;
+            }
+        }
+        return -1;
+    }
+    
+    public static void postOrder(BinaryTreeNode<Integer> root , ArrayList<Integer> arr){
+          if (root == null) {
+            return;
+        }
+        
+        postOrder(root.left, arr);
+        postOrder(root.right, arr);
+        
+        arr.add(root.data);
+        
+        
+    }
+
+}
