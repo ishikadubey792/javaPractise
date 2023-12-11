13b32466fca2a61aa7d0956af032886a32d5ba9d
Ishika <128132667+ishikadubey792@users.noreply.github.com>
2023-12-11 07:19:49 +0000

    leetcode 500

---
+++ keyboardRow.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        List<String> list = new LinkedList<>();
+        for(String word : words){
+            int[] row = new int[3];
+            for(char ch : word.toLowerCase().toCharArray()){
+                if(row1.indexOf(ch)!= -1){
+                    row[0] = 1;
+                }else if(row2.indexOf(ch)!= -1){
+                    row[1] = 1;
+                }else if(row3.indexOf(ch)!= -1){
+                    row[2] = 1;
+                }
+            }
+            int sum = row[0] + row[1] + row[2];
+            if(sum == 1){
+                list.add(word);
+            }
+        }
+
+        String[] strArray = new String[list.size()];
+        int i = 0;
+        for(String word : list){
+            strArray[i++] = word;
+        }
+
+        return strArray;
+       
+    }
+}
